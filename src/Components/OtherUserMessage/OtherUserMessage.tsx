0881e57cbf44fd43404cbdf0180c8f2cf1d34fc4
Luan Henrique <152590653+Lhenrick@users.noreply.github.com>
2025-01-23 11:28:41 -0300

    user and other users messages in different sides

--- src/Components/OtherUserMessage/OtherUserMessage.tsx
+++ src/Components/OtherUserMessage/OtherUserMessage.tsx
@@ -4,14 +4,14 @@ export default function OtherUserMessage() {
   return (
     <div className="otherMessage-container">
       <div className="otherMessage-container__card">
+        <div className="otherMessage__content">
+          <p>Other Message</p>
+        </div>
         <img
           className="otherUser__picture"
           src="./Img/kisaki.jpg"
           alt="User profile picture"
         />
-        <div className="otherMessage__content">
-          <p>Other Message</p>
-        </div>
       </div>
     </div>
   );
